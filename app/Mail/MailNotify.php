2c9e7628735fcf8bbd943dafcec05f3d3cc545f7
Hanh <Hanh@PNVL70.pn.com.vn>
2022-07-26 10:12:00 +0700

    Update index.php for heroku

--- app/Mail/MailNotify.php
+++ app/Mail/MailNotify.php
@@ -10,24 +10,11 @@ use Illuminate\Queue\SerializesModels;
 class MailNotify extends Mailable
 {
     use Queueable, SerializesModels;
-
-    public $data;
-   /**
-    * Create a new data instance.
-    *
-    * @return void
-    */
-
    public function __construct($data)
    {
        $this->data = $data;
    }
 
-   /**
-    * Build the message.
-    *
-    * @return $this
-    */
    public function build()
    {
        return $this->from('shopbanhkeo@gmail.com')
